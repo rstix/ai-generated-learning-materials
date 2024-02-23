4a2990d50b5663bfa39933286200b10e0b00a7b1
Roman Stix <rsonroad@gmail.com>
2024-02-23 11:39:57 +0100

    api hooked

--- index.js
+++ index.js
@@ -13,21 +13,27 @@
 
 // main()
 
+console.log("ash")
+
 import OpenAI from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const openai = new OpenAI({
-  apiKey: process.env['OPEN_AI_API_KEY'], // This is the default and can be omitted
-});
+console.log(process.env.OPENAI_API_KEY)
 
-async function main() {
-  const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: 'user', content: 'Say this is a test' }],
-    model: 'gpt-3.5-turbo',
-  });
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
+// });
 
-  // const completionText = completion.data.choices[0].message.content;
+// async function main() {
+//   const chatCompletion = await openai.chat.completions.create({
+//     messages: [{ role: 'user', content: 'Say this is a test' }],
+//     model: 'gpt-3.5-turbo',
+//   });
 
-  console.log(chatCompletion)
-}
+//   // const completionText = completion.data.choices[0].message.content;
 
-main();
+//   console.log(chatCompletion)
+// }
+
+// main();
