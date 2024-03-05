7a2734039fd170aae8836aae8a03f66882be5614
Roman Stix <rsonroad@gmail.com>
2024-03-05 23:50:33 +0100

    ranaming

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [ "./*.{html,js}"],
-	theme: {
-	  extend: {},
-	  fontFamily: {
-		'indie': ["Handlee", 'sans-serif'],
-	  },
-	  colors: {
-		transparent: 'transparent',
-		current: 'currentColor',
-		'white': '#ffffff',
-		'black': '#000000',
-		'yellow': '#fca311',
-		'darkBlue': '#14213d',
-		'lightGray': '#e5e5e5',
-	  },
-	},
-	plugins: [],
-  }
+  content: ['./*.{html,js}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      indie: ['Lato', 'sans-serif'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      black: '#000000',
+      yellow: '#fca311',
+      darkBlue: '#14213d',
+      lightGray: '#e5e5e5',
+    },
+  },
+  plugins: [],
+};
