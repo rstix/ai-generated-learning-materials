4f0b6ec14ad7896f2e9e1da8bfee86ed4d7064dc
Roman Stix <rsonroad@gmail.com>
2024-03-05 20:00:42 +0100

    students added

--- client/js/main.js
+++ client/js/main.js
@@ -1,45 +1,41 @@
-const btns = document.querySelectorAll("#btns button")
-
-btns[0].style.background = '#e5e5e5';
-
-btns.forEach(e => {
-  e.addEventListener('click', () => {
-    removeStylesFromElements(btns);
-    e.style.background = '#e5e5e5';
-  });
-});
-
 const removeStylesFromElements = (elements) => {
-  elements.forEach(e => e.removeAttribute('style'));
-}
+  elements.forEach((e) => e.removeAttribute('style'));
+};
+
+// btns[0].style.background = '#e5e5e5';
+
+const btns = document.querySelectorAll('#btns a');
+
+btns.forEach((e) => {
+  if (location.pathname.includes(e.getAttribute('href'))) {
+    console.log(location.pathname);
+    e.style.background = '#e5e5e5';
+  }
+});
 
 const openAccordion = (accordion) => {
-  accordion.classList.toggle("is-open");
+  accordion.classList.toggle('is-open');
 
-    let content = accordion.nextElementSibling;
+  let content = accordion.nextElementSibling;
 
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
+  if (content.style.maxHeight) {
+    content.style.maxHeight = null;
+  } else {
+    content.style.maxHeight = content.scrollHeight + 'px';
+  }
+};
+
+const accordionBtns = document.querySelectorAll('.accordion');
+
+if (accordionBtns.length) {
+  setTimeout(() => {
+    openAccordion(accordionBtns[0]);
+  }, 800);
+
+  accordionBtns.forEach((accordion) => {
+    accordion.addEventListener('click', () => openAccordion(accordion));
+  });
 }
-
-const accordionBtns = document.querySelectorAll(".accordion");
-
-setTimeout(() => {
-  openAccordion(accordionBtns[0])
-}, 800)
-
-accordionBtns.forEach((accordion) => {
-  accordion.addEventListener('click', ()=> openAccordion(accordion))
-});
-
-
-
-
-
-
 
 // function onSubmit(e) {
 //   e.preventDefault();
@@ -53,8 +49,6 @@ accordionBtns.forEach((accordion) => {
 //   if(matchingCheckbox.checked){
 // 	matching.style.display = "block"
 //   }
-
-
 
 // //   document.querySelector(".msg").textContent = "";
 // //   document.querySelector("#image").src = "";
@@ -113,44 +107,38 @@ accordionBtns.forEach((accordion) => {
 
 // document.querySelector("#image-form").addEventListener("submit", onSubmit);
 
-
-
-
-
-
-
 //  function enableDragSort(listClass) {
 // 	const sortableLists = document.getElementsByClassName(listClass);
 // 	Array.prototype.map.call(sortableLists, (list) => {enableDragList(list)});
 //   }
-  
+
 //   function enableDragList(list) {
 // 	Array.prototype.map.call(list.children, (item) => {enableDragItem(item)});
 //   }
-  
+
 //   function enableDragItem(item) {
 // 	item.setAttribute('draggable', true)
 // 	item.ondrag = handleDrag;
 // 	item.ondragend = handleDrop;
 //   }
-  
+
 //   function handleDrag(item) {
 // 	const selectedItem = item.target,
 // 		  list = selectedItem.parentNode,
 // 		  x = event.clientX,
 // 		  y = event.clientY;
-	
+
 // 	selectedItem.classList.add('drag-sort-active');
 // 	let swapItem = document.elementFromPoint(x, y) === null ? selectedItem : document.elementFromPoint(x, y);
-	
+
 // 	if (list === swapItem.parentNode) {
 // 	  swapItem = swapItem !== selectedItem.nextSibling ? swapItem : swapItem.nextSibling;
 // 	  list.insertBefore(selectedItem, swapItem);
 // 	}
 //   }
-  
+
 //   function handleDrop(item) {
 // 	item.target.classList.remove('drag-sort-active');
 //   }
-  
+
 //   (()=> {enableDragSort('drag-sort-enable')})();
